4078fdb106b4e4cf88f6d33e42ba7afc969b1962
IRIS <dongquanthaito@gmail.com>
2024-07-15 22:17:35 +0800

    update source

--- functions/function.js
+++ functions/function.js
@@ -4,6 +4,8 @@ module.exports = {
   updateSource: async () => {
     console.log('----- TIẾN TRÌNH CẬP NHẬT -----');
     nodeCmd.runSync('git reset --hard');
+    nodeCmd.runSync('git config --global --add safe.directory C:/ui_automator_v2');
+
     const pull = nodeCmd.runSync('git pull');
     console.log(pull);
     if (pull.data.includes('Already up to date')) return;
