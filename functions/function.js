7dcb6e9a95d1366ca01a87cde293053d27a756f0
IRIS <dongquanthaito@gmail.com>
2024-07-25 18:14:45 +0800

    .

--- functions/function.js
+++ functions/function.js
@@ -3,13 +3,13 @@ const nodeCmd = require('../helpers/nodeCmdHelper');
 module.exports = {
   updateSource: async () => {
     console.log('----- TIẾN TRÌNH CẬP NHẬT -----');
-    // nodeCmd.runSync('git config --global --add safe.directory C:/ui_automator_v2');
-    // nodeCmd.runSync('git reset --hard');
+    nodeCmd.runSync('git config --global --add safe.directory C:/ui_automator_v2');
+    nodeCmd.runSync('git reset --hard');
 
-    // const pull = nodeCmd.runSync('git pull');
-    // console.log(pull);
-    // if (pull.data.includes('Already up to date')) return;
+    const pull = nodeCmd.runSync('git pull');
+    console.log(pull);
+    if (pull.data.includes('Already up to date')) return;
 
-    // nodeCmd.run('pm2 restart ui');
+    nodeCmd.run('pm2 restart ui');
   }
 };
