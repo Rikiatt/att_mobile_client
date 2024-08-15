d283d67dae64ba3823803ec35f53dd2c1b366596
IRIS <dongquanthaito@gmail.com>
2024-08-15 15:43:54 +0800

    add: gitignore

---
+++ views/portal_ui_automator/src/api/adb.js
@@ -0,0 +1,35 @@
+import axios from '../utils/axios';
+
+export const endpoints = {
+  key: 'adb',
+  listDevice: '/list-devices',
+  actionADB: '/action-adb'
+};
+
+export async function getListDevice() {
+  try {
+    const url = endpoints.key + endpoints.listDevice;
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return {
+      status: false,
+      msg: error.message
+    };
+  }
+}
+
+export async function postActionADB(data) {
+  try {
+    const url = endpoints.key + endpoints.actionADB;
+    const response = await axios.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return {
+      status: false,
+      msg: error.message
+    };
+  }
+}
