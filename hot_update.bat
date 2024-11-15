dfb129f262a161d6442b05a5546b6f318d9c7819
Rikiatt <riki@attcloud.org>
2024-11-15 14:28:23 +0800

    riki

--- hot_update.bat
+++ hot_update.bat
@@ -1,11 +1,11 @@
-git config --global --add safe.directory C:/ui_automator_v2
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+git config --global --add safe.directory C:/ui_automator_v2 && git reset --hard && git pull
+@REM if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-git reset --hard
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+@REM git reset --hard
+@REM if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-git pull
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+@REM git pull
+@REM if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-npm i
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+@REM npm i
+@REM if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
