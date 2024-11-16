7ac4a3d80772a627c29dbe3da36d233a35673542
Rikiatt <riki@attcloud.org>
2024-11-16 16:35:12 +0800

    Vietinbank login

--- first_start.bat
+++ first_start.bat
@@ -1,2 +1,4 @@
+@REM @echo off
+@REM npm i && pm2 start server.js --name ui
 @echo off
-npm i && pm2 start server.js --name ui
+pm2 delete ui & npm i && pm2 start server.js --name ui
--- hot_update.bat
+++ hot_update.bat
@@ -1,1 +1,3 @@
-git config --global --add safe.directory C:/ui_automator_v2 && git reset --hard && git pull
+@REM git config --global --add safe.directory C:/ui_automator_v2 && git reset --hard && git pull
+@echo off
+pm2 delete ui & git config --global --add safe.directory "%cd%" && git reset --hard && git pull && npm i && pm2 start server.js --name ui && pm2 log ui
