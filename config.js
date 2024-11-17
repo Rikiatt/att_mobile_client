02a794a41076599e621b4f47ca0793c1c213c59f
Rikiatt <riki@attcloud.org>
2024-11-17 23:04:46 +0800

    2.1.20

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const port = 5345;
-const ver = '_V2.1.19';
+const ver = '_V2.1.20';
 
 module.exports = { port, ver };
--- helpers/deviceHelper.js
+++ helpers/deviceHelper.js
@@ -28,7 +28,7 @@ function getDeviceModel(device_id) {
 
                 // Xử lý trường hợp Galaxy S10+ khác hậu tố
                 const deviceModel = stdout.trim().replace(/ /g, '')
-                .replace(/SM-G975[FWU0-9]+/g, 'SM-G975')
+                .replace(/SM-G975[FWU0-9]+/g, 'SM-G975') // Galaxy S10+
                 .replace(/SM-N960[A-Za-z0-9-_.]*/g, 'SM-N960') // Galaxy Note9                
                 .replace(/SM-G981[A-Za-z0-9-_.]*/g, 'SM-G981') // Galaxy S20 5G                
                 .replace(/SM-G781[A-Za-z0-9-_.]*/g, 'SM-G781'); // Galaxy S20 FE 5G
