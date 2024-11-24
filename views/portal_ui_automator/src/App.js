1dc1ee4b152c2cf9f8009fe4064dc0361997c77d
yakun9it <yakun9it@gmail.com>
2024-11-24 08:32:57 +0700

    change frontend

--- views/portal_ui_automator/src/App.js
+++ views/portal_ui_automator/src/App.js
@@ -40,7 +40,7 @@ import {
   WifiTethering
 } from '@mui/icons-material';
 
-import { swalToast, swalQuestionConfirm, swalInputText, swalInfoChooseText } from './utils/swal';
+import { swalToast, swalQuestionConfirm, swalInputText, swalInfoChooseText, swalQuestionConfirms } from './utils/swal';
 import { connect, connectTcpIp, disconnectTcpIp, enter, home, typePortKey, typeText } from './services/handle.service';
 import { blue } from '@mui/material/colors';
 import HandleBidv from './sections/bank_handle/HandleBidv';
@@ -124,7 +124,7 @@ function App() {
               <SetupIP setMutate={setMutate} />
               <Typography variant="button" fontWeight="bold" color="#172B4D">IP: {ipPublic}</Typography>
             </Stack>
-            <SetupConnect setMutate={setMutate} seting={seting} />
+            <SetupConnect setMutate={setMutate} seting={seting} setSeting={setSeting} />
           </Stack>
           <Divider sx={{ mt: 2 }} />
 
@@ -385,7 +385,7 @@ const actionsDial = [
   { icon: <PowerSettingsNew color="error" />, name: 'Restart tool', typeHandle: 'restart' }
 ];
 
-function SetupConnect({ setMutate, seting }) {
+function SetupConnect({ setMutate, seting, setSeting }) {
   let att_connect = seting.att?.connected || false;
   let org_connect = seting.org?.connected || false;
 
@@ -408,20 +408,43 @@ function SetupConnect({ setMutate, seting }) {
   }
 
   const handleConnect = async (type, disconnect) => {
-    const result = await swalInfoChooseText((disconnect ? 'Ngắt kết nối ' : 'Kết nối nhận mã QR từ ') + type.toUpperCase());
-    if (result) {
-      const data = { type, disconnect };
-      const result = await connectEndpoint(data);
-      if (result?.valid == true) {
-        return swalToast('success', 'Thành công');
-      } else {
-        return swalToast('error', "Lỗi hệ thống");
+    if (disconnect) {
+      const result = await swalInfoChooseText('Ngắt kết nối ' + type.toUpperCase());
+      if (result) {
+        const data = { type, disconnect };
+        const result = await connectEndpoint(data);
+        if (result?.valid == true) {
+          return swalToast('success', 'Thành công');
+        } else {
+          return swalToast('error', "Lỗi hệ thống");
+        }
       }
+    } else {
+      const acction = await swalQuestionConfirms('warning', 'Thao tác kết nối', 'Thay Url ' + type.toUpperCase(), 'Kết nối ' + type.toUpperCase(), 'Hủy');
+
+      if (acction) {
+        if (acction == 'confirm') { return handleEndpoint(type) };
+        const data = { type, disconnect };
+        const result = await connectEndpoint(data);
+        if (result?.valid == true) {
+          setSeting((pre) => ({ ...pre, connect: type, [type]: { ...pre[type], connected: true } }))
+          return swalToast('success', 'Thành công');
+        } else {
+          setSeting((pre) => ({ ...pre, connect: '', att: { ...pre.att, connected: false }, org: { ...pre.org, connected: false } }))
+          return swalToast('error', "Lỗi hệ thống");
+        }
+      }
+
     }
   }
   return (
     <Stack direction="row" alignItems="center" spacing={2}>
-      <FormControlLabel sx={{ background: seting.connect == 'org' ? '#a9ffa9' : 'unset', p: "0 8px", borderRadius: 12 }} control={
+      <FormControlLabel sx={{
+        background: seting.connect == 'org' ? (seting.org.connected == true ? '#5ced5c' : 'red') : 'unset',
+        p: "0 8px",
+        borderRadius: 12,
+        boxShadow: "rgba(50, 50, 93, 0.25) 0px 30px 60px - 12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset"
+      }} control={
         !seting.org
           ? (<Tooltip title="ORG - Cấu hình link truy cập" arrow>
             <IconButton size="small" onClick={() => handleEndpoint('org')}>
@@ -430,7 +453,12 @@ function SetupConnect({ setMutate, seting }) {
           </Tooltip>)
           : (seting.org?.endpoint && <Switch color="primary" checked={org_connect} onChange={() => handleConnect('org', org_connect)} />)
       } label="ORG" labelPlacement="start" />
-      <FormControlLabel sx={{ background: seting.connect == 'att' ? '#a9ffa9' : 'unset', p: "0 8px", borderRadius: 12 }} control={
+      <FormControlLabel sx={{
+        background: seting.connect == 'att' ? (seting.att.connected ? '#5ced5c' : 'red') : 'unset',
+        p: "0 8px",
+        borderRadius: 12,
+        boxShadow: "rgba(50, 50, 93, 0.25) 0px 30px 60px - 12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset"
+      }} control={
         !seting.att
           ? (<Tooltip title="ATTPAY+ - Cấu hình link truy cập" arrow>
             <IconButton size="small" onClick={() => handleEndpoint('att')}>
