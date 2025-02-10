106e97b72da6a9b6905b0ce03d02f0d21de90cd2
Rikiatt <riki@attcloud.org>
2025-02-10 16:59:57 +0800

    .

--- functions/endpoint.js
+++ functions/endpoint.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const io = require('socket.io-client');
 const { delay } = require('../helpers/functionHelper');
-const { listDevice, sendFile, delImg } = require('./adb.function');
+// const { listDevice, sendFile, delImg } = require('./adb.function');
+const { sendFile, delImg } = require('./adb.function');
 const { transToQr, downloadQr, setDataJson, getDataJson, getIpPublic } = require('./function');
 let currentSocket = null;
 const adbPath = path.join(__dirname, '../platform-tools', 'adb.exe');
@@ -19,29 +20,92 @@ const seconds = String(date.getSeconds()).padStart(2, '0');
 const filename = `${year}${month}${day}_${hours}${minutes}${seconds}`;
 let qrDevicePath = '/sdcard/DCIM/Camera/' + filename + '.jpg';
 
-// async function listDevice() {
-//   try {
-//     const devices = await client.listDevices();
-//     for (let device of devices) {
-//       const [screenSize, nameDevice, androidVersion, model] = await Promise.all([
-//         getScreenSize(device.id),
-//         getNameDevice(device.id),
-//         getAndroidVersion(device.id),
-//         getModel(device.id)
-//       ])
+const getScreenSize = async (device_id) => {
+  try {
+    // Thực thi lệnh `wm size` trên thiết bị
+    const output = await client.shell(device_id, 'wm size');
+    const resultBuffer = await adb.util.readAll(output);
+    const result = resultBuffer.toString();
 
-//       device.screenSize = screenSize;
-//       device.nameDevice = nameDevice;
-//       device.androidVersion = androidVersion;
-//       device.model = model;
-//     }
-//     console.log("Danh sách thiết bị ", devices?.length);
-//     return devices;
-//   } catch (error) {
-//     console.error('Error getting connected devices:', error);
-//     return [];
-//   }
-// }
+    // Sử dụng regex để tìm kiếm Override size và Physical size
+    const overrideSizeMatch = result.match(/Override size: (\d+x\d+)/);
+    const physicalSizeMatch = result.match(/Physical size: (\d+x\d+)/);
+
+    // Nếu có Override size, trả về nó, nếu không trả về Physical size
+    if (overrideSizeMatch) {
+      return overrideSizeMatch[1];
+    } else if (physicalSizeMatch) {
+      return physicalSizeMatch[1];
+    } else {
+      return '';
+    }
+  } catch (error) {
+    console.error('Error getting screen size:', error);
+    return '';
+  }
+};
+
+const getNameDevice = async (device_id) => {
+  try {
+    const output = await client.shell(device_id, 'dumpsys bluetooth_manager | grep name');
+    const resultBuffer = await adb.util.readAll(output);
+    const result = resultBuffer.toString();
+    const match = result.match(/name:\s*(.*)\r?\n/);
+    const name = match ? match[1].trim() : '';
+    return name;
+  } catch (error) {
+    console.error('Error getting Bluetooth device name:', error);
+    return '';
+  }
+};
+
+const getAndroidVersion = async (device_id) => {
+  try {
+    const output = await client.shell(device_id, 'getprop ro.build.version.release');
+    const resultBuffer = await adb.util.readAll(output);
+    const result = parseInt(resultBuffer.toString().trim());
+    return result;
+  } catch (error) {
+    console.error('Error getting Android version:', error);
+    return '';
+  }
+};
+
+const getModel = async (device_id) => {
+  try {
+    const output = await client.shell(device_id, 'getprop ro.product.model');
+    const resultBuffer = await adb.util.readAll(output);
+    const result = resultBuffer.toString().trim();
+    return result;
+  } catch (error) {
+    console.error('Error getting model:', error);
+    return '';
+  }
+};
+
+async function listDevice() {
+  try {
+    const devices = await client.listDevices();
+    for (let device of devices) {
+      const [screenSize, nameDevice, androidVersion, model] = await Promise.all([
+        getScreenSize(device.id),
+        getNameDevice(device.id),
+        getAndroidVersion(device.id),
+        getModel(device.id)
+      ])
+
+      device.screenSize = screenSize;
+      device.nameDevice = nameDevice;
+      device.androidVersion = androidVersion;
+      device.model = model;
+    }
+    console.log("Danh sách thiết bị ", devices?.length);
+    return devices;
+  } catch (error) {
+    console.error('Error getting connected devices:', error);
+    return [];
+  }
+}
 
 module.exports = {
   qrDevicePath,
