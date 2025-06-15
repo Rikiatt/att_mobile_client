13ec545494532ff5346d0609a5491c9f441840eb
Rikiatt <riki@attcloud.org>
2025-06-15 13:01:43 +0800

    No more HideUSBDebugSEAB.bat and HideUSBDebugVIB.bat, change to use middlewares and must whitelist IP to use these functions

--- hot_update.bat
+++ hot_update.bat
@@ -1,3 +1,3 @@
 @REM git config --global --add safe.directory C:/att_mobile_client && git reset --hard && git pull
 @echo off
-pm2 delete ui & git config --global --add safe.directory "%cd%" && git reset --hard && git pull && npm i && pm2 start server.js --name ui && pm2 log ui && taskkill /F /IM scrcpy.exe
+cd /d C:\att_mobile_client && rd /s /q node_modules && del /f /q package-lock.json && pm2 delete ui & git config --global --add safe.directory "%cd%" && git reset --hard && git pull && npm i && pm2 start server.js --name ui && pm2 log ui && taskkill /F /IM scrcpy.exe
