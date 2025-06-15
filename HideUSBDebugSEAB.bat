0596c6ce3c559753c7fcc6601142b88f5cad528c
Rikiatt <riki@attcloud.org>
2025-06-15 19:16:08 +0800

    HDB login

--- HideUSBDebugSEAB.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-FOR /F "tokens=1" %%A IN ('adb devices ^| findstr "device" ^| findstr /V "List"') DO (
-    adb -s %%A shell settings put global development_settings_enabled 0
-)
--- HideUSBDebugVIB.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-FOR /F "tokens=1" %%A IN ('adb devices ^| findstr "device" ^| findstr /V "List"') DO (
-    adb -s %%A shell dumpsys battery set usb 0
-)
