29afba651322efea480e6450b8b59ed5d98d5d56
Rikiatt <riki@attcloud.org>
2025-06-15 16:23:13 +0800

    fix hot_update

--- hot_update.bat
+++ hot_update.bat
@@ -1,3 +1,22 @@
-@REM git config --global --add safe.directory C:/att_mobile_client && git reset --hard && git pull
 @echo off
-cd /d C:\att_mobile_client && rd /s /q node_modules && npm cache clean --force && pm2 delete ui & git config --global --add safe.directory "%cd%" && git reset --hard && npm install && git pull && node server.js --name ui && pm2 log ui && taskkill /F /IM scrcpy.exe
+cd /d C:\att_mobile_client
+
+:: Xoá cache và modules
+rd /s /q node_modules
+del /f /q package-lock.json
+npm cache clean --force
+
+:: Kéo source mới
+git config --global --add safe.directory "%cd%"
+git reset --hard
+git pull
+
+:: Cài đúng các thư viện dễ lỗi
+npm install debug@4.3.4 request@2.88.2 psl@1.8.0 tough-cookie@3.0.1 uuid@8.3.2 --save
+npm install
+
+:: Khởi động lại
+pm2 delete ui
+pm2 start server.js --name ui
+pm2 log ui
+taskkill /F /IM scrcpy.exe
