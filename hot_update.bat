fa714967cb12f34a1bf9c10c0902176285c1e3aa
Rikiatt <riki@attcloud.org>
2025-06-15 17:08:38 +0800

    the end of fix hot_update

--- hot_update.bat
+++ hot_update.bat
@@ -9,13 +9,10 @@ npm cache clean --force
 git config --global --add safe.directory "%cd%"
 git reset --hard
 git pull
-
-:: Cài đúng các thư viện dễ lỗi
 npm install
-npm install debug@4.3.4 request@2.88.2 psl@1.8.0 tough-cookie@3.0.1 uuid@8.3.2 --save
 
 :: Khởi động lại
 pm2 delete ui
+taskkill /F /IM scrcpy.exe
 pm2 start server.js --name ui
 pm2 log ui
-taskkill /F /IM scrcpy.exe
